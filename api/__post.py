1d59d6665eb65c4228350f534dc6d013e95c51de
Arpit <arpitsengar99@gmail.com>
2023-12-08 13:55:37 +0530

    upload api directory

---
+++ api/__post.py
@@ -0,0 +1,51 @@
+import requests
+from flask import Flask, request, jsonify
+from __constants import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return jsonify({"NOTE": "Hi."})
+
+@app.route("/send", methods=["POST"])
+def send_data():    
+    try:
+        username = request.json.get("username")  
+        message = request.json.get("message") 
+        data = f"{username}: {message}"
+        
+        if not data:
+            return jsonify({"error": "Data not provided"}), 400
+
+        post_message_to_server(data)
+        return jsonify({"success": True})
+    except Exception as e:
+        print(e)
+        return jsonify({"error": "Internal server error"}), 500
+
+def post_message_to_server(message):
+    try:
+        send_message_data = {
+            "content": message,
+        }
+
+        response = requests.post(send_message_url, json=send_message_data, headers=headers)
+        if response.status_code != 200:
+            print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
+    except Exception as e:
+        print("Error sending message:", str(e))
+
+# def read_message_from_server():
+#     response = requests.get(f'https://discord.com/api/v10/channels/{CHANNEL_ID}/messages', headers=headers, params={'limit': 1})
+#     data = response.json()
+
+#     if response.status_code == 200 and data:
+#         last_message = data[0]['content']
+#         print(f'Last Message: {last_message}')
+#     else:
+#         print(f'Failed to fetch last message. Status code: {response.status_code}, Response: {response.text}')
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
