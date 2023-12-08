674f32841569625051a8fd99ff0897e8829459ce
Arpit <arpitsengar99@gmail.com>
2023-12-08 15:17:28 +0530

    merge post and get request

--- api/main.py
+++ api/main.py
@@ -18,18 +18,35 @@ app = Flask(__name__)
 def index():
     return jsonify({"NOTE": "Hi."})
 
-@app.route("/send", methods=["POST"])
-def send_data():    
+@app.route("/data", methods=["POST", "GET"])
+def send_data():
     try:
-        username = request.json.get("username")  
-        message = request.json.get("message") 
-        data = f"{username}: {message}"
-        
-        if not data:
-            return jsonify({"error": "Data not provided"}), 400
+        if request.method == "POST":
+            username = request.json.get("username")
+            message = request.json.get("message")
+            data = f"{username}: {message}"
 
-        post_message_to_server(data)
-        return jsonify({"success": True})
+            if not data:
+                return jsonify({"error": "Data not provided"}), 400
+
+            post_message_to_server(data)
+            return jsonify({username: message})
+
+        elif request.method == "GET":
+            response = requests.get(f'https://discord.com/api/v10/channels/{CHANNEL_ID}/messages', headers=headers, params={'limit': 1})
+            data = response.json()
+
+            if response.status_code == 200 and data:
+                last_message = data[0]['content']
+                return jsonify({"message": last_message})
+            else:
+                print(f'Failed to fetch last message. Status code: {response.status_code}, Response: {response.text}')
+
+            if not data:
+                return jsonify({"error": "Couldn't fetch data"}), 400
+
+            return jsonify({"success": True})
+
     except Exception as e:
         print(e)
         return jsonify({"error": "Internal server error"}), 500
@@ -45,17 +62,6 @@ def post_message_to_server(message):
             print(f"Failed to send message. Status code: {response.status_code}, Response: {response.text}")
     except Exception as e:
         print("Error sending message:", str(e))
-
-# def read_message_from_server():
-#     response = requests.get(f'https://discord.com/api/v10/channels/{CHANNEL_ID}/messages', headers=headers, params={'limit': 1})
-#     data = response.json()
-
-#     if response.status_code == 200 and data:
-#         last_message = data[0]['content']
-#         print(f'Last Message: {last_message}')
-#     else:
-#         print(f'Failed to fetch last message. Status code: {response.status_code}, Response: {response.text}')
-    
 
 if __name__ == "__main__":
     app.run()
